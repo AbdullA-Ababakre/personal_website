b3d03fa9adb050fc915132463244fed089f172ca
AbdullA-Ababakre <abdullaszu@gmail.com>
2023-08-01 18:07:26 -0400

    feat: footer function

--- src/components/AboutMe/index.js
+++ src/components/AboutMe/index.js
@@ -1,19 +1,22 @@
-import React, { useEffect } from 'react';
-import styles from './styles.module.css';
+import React, { useEffect } from "react";
+import styles from "./styles.module.css";
 import { FaLinkedin, FaGithub, FaTwitter } from "react-icons/fa";
 import { FiMail } from "react-icons/fi";
 import { IoDocumentTextOutline } from "react-icons/io5";
 import MyPicture from "@site/static/img/picture_me2.jpeg";
-import Typed from 'typed.js';
-
+import Typed from "typed.js";
 
 export default function HomepageFeatures() {
-
   const el = React.useRef(null);
 
   useEffect(() => {
     const typed = new Typed(el.current, {
-      strings: ['<i>A fullstack javascript engineer</i>', 'A Proud Uyghur', 'An Engineer who tried several startUps', 'A new Comer to USA'],
+      strings: [
+        "<i>A fullstack javascript engineer</i>",
+        "A Proud Uyghur",
+        "An Engineer who tried several startUps",
+        "A new Comer to USA",
+      ],
       typeSpeed: 50,
     });
 
@@ -37,27 +40,42 @@ export default function HomepageFeatures() {
             </figure>{" "}
             <h1 className={styles.about_me__info__title}>
               Hey!I 'm{" "}
-              <span className={styles.text__purple}> Abdulla Ababakre </span>&nbsp;{" "}
-              <span className={styles.wave}> 👋 </span>{" "}
+              <span className={styles.text__purple}> Abdulla Ababakre </span>
+              &nbsp; <span className={styles.wave}> 👋 </span>{" "}
             </h1>{" "}
             <p className={styles.about_me__info__para}>
-              I 'm  {" "}
+              I 'm{" "}
               {/* <strong className={styles.text__purple}>
                 fullstack javascript engineer{" "}
               </strong>{" "} */}
-              <span className={styles.text__purple + " " + styles.typed} ref={el} />
+              <span
+                className={styles.text__purple + " " + styles.typed}
+                ref={el}
+              />
             </p>{" "}
             <div className={styles.about_me__links}>
-              <a href="https://www.linkedin.com/in/abdulla-ababakre-52a3a3228/" className={styles.about_me__link}>
+              <a
+                href="https://www.linkedin.com/in/abdulla-ababakre-52a3a3228/"
+                className={styles.about_me__link}
+              >
                 <FaLinkedin size={24} />{" "}
               </a>{" "}
-              <a href="https://github.com/AbdullA-Ababakre?tab=repositories" className={styles.about_me__link}>
+              <a
+                href="https://github.com/AbdullA-Ababakre?tab=repositories"
+                className={styles.about_me__link}
+              >
                 <FaGithub size={24} />{" "}
               </a>{" "}
-              <a href="https://twitter.com/aqilgin" className={styles.about_me__link}>
+              <a
+                href="https://twitter.com/aqilgin"
+                className={styles.about_me__link}
+              >
                 <FaTwitter size={24} />{" "}
               </a>{" "}
-              <a href="mailto:abdullaszu@gmail.com" className={styles.about_me__link}>
+              <a
+                href="mailto:abdullaszu@gmail.com"
+                className={styles.about_me__link}
+              >
                 <FiMail size={24} />{" "}
               </a>{" "}
               <a href="" target="_blank" className={styles.about_me__link}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,14 +4,13 @@ import Layout from "@theme/Layout";
 import AboutMe from "@site/src/components/AboutMe";
 import Projects from "@site/src/components/Projects";
 import Languages from "../components/Languages";
-import Footer from "@site/src/components/Footer";
 
 export default function Home() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      description="web development blog and portfolio"
     >
       <AboutMe />
       <Languages />
